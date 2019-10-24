2c4c7567f83f7d6936866d4ec3f183d7c5b5d15c
Tom Gage <tomgage01@gmail.com>
2019-10-25 00:51:50 +0100

    added deadline class and gui mockup

---
+++ DeadlineTracker/src/deadlinetracker/Deadline.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deadlinetracker;
+
+/**
+ *
+ * @author Tom
+ */
+public class Deadline {
+    String title = null;
+    String details = null;
+}
